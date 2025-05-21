f988ed3b10cd631cf44c6ec16cf343fcfb24d0c7
Akhil Waghmare <akhilwaghmare@gmail.com>
2025-05-21 14:23:15 -0700

    Basic structure and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Starter code
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <section className="bg-yellow-500 h-screen w-full flex items-center justify-center">
+        <h1 className="text-white text-8xl font-bold">{"Hi, I'm Charles"}</h1>
+      </section>
+      <section className="relative h-screen w-full flex items-center justify-center">
+        <div className="absolute inset-0 w-full h-full object-cover -z-10">
+          <Image
+            className="w-full h-full object-cover"
+            src="/bg-image.jpg"
+            alt="Racecar driver"
+            fill
+          />
+          <div className="absolute inset-0 bg-black/70 w-full h-full object-cover" />
+        </div>
+        <h1 className="uppercase font-bold text-white text-8xl text-nowrap">
+          {"It's more about hard work than talent"}
+        </h1>
+      </section>
     </div>
   );
 }
